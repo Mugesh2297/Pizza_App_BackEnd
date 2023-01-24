62b598d4ed927da8aff4749585964deade9741e2
Mugesh <mugeshkumark22@gmail.com>
2023-01-24 12:35:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const connectDB = require('./config/config');
+
+require('colors');
+const morgan = require('morgan');
+
+//config dotenv
+dotenv.config()
+
+
+//connection mongodb
+connectDB();
+
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'));
+// app.use(cors({ origin: "http://localhost:3000/"}))
+
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+//route
+app.use("/api/pizzas", require("./routes/pizzaRoutes"));
+app.use("/api/users", require('./routes/userRoutes'));
+app.use("/api/orders", require('./routes/orderRoutes'))
+app.get("/",(req,res) =>{
+    res.send('<h1>Take a Risk</h1>');
+})
+
+
+const port =process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}!`)
+});
+
