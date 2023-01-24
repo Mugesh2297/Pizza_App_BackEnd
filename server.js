9b7120373725f7cc3c6f3b32ab034ef588bcfe9f
Mugesh <mugeshkumark22@gmail.com>
2023-01-24 12:43:30 +0530

    link changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(morgan('dev'));
 
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://pizzaapp22.netlify.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
